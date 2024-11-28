a15ee0615908648d536f23398797c37932c58cbe
unknown <ngps2046@gmail.com>
2024-11-28 16:18:41 +0545

    Project setup and completed roles CRUD api

---
+++ src/roles/roles.service.ts
@@ -0,0 +1,65 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaClientValidationError } from '@prisma/client/runtime/library';
+
+@Injectable()
+export class RolesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createRoleDto: CreateRoleDto) {
+    await this.checkIfRoleExists(createRoleDto.name);
+    return this.prismaService.role.create({ data: createRoleDto });
+  }
+
+  async findAll() {
+    return this.prismaService.role.findMany();
+  }
+
+  async findOne(id: number) {
+    return this.getRole(id);
+  }
+
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    await this.getRole(id);
+    await this.checkIfRoleExists(updateRoleDto.name, id);
+    return this.prismaService.role.update({
+      where: { id },
+      data: updateRoleDto,
+    });
+  }
+
+  async remove(id: number) {
+    await this.getRole(id);
+    return this.prismaService.role.delete({ where: { id } });
+  }
+
+  private async getRole(id: number) {
+    const role = await this.prismaService.role.findFirst({ where: { id } });
+
+    if (!role) {
+      throw new NotFoundException('Role not found');
+    }
+    return role;
+  }
+  private async checkIfRoleExists(name: string, id?: number) {
+    const doesRoleExists = await this.prismaService.role.findFirst({
+      where: { name: name },
+    });
+
+    if (doesRoleExists) {
+      if (id && doesRoleExists?.id == id) {
+        // this is update case
+        throw new BadRequestException(`Role ${name} already exists`);
+      } else if (!id) {
+        //this is create case
+        throw new BadRequestException(`Role ${name} already exists`);
+      }
+    }
+  }
+}
